13a615a43364aa65b999956cf6ee821704905441
Alireza Rouzitalab <alireza@Alirezas-MacBook-Air.local>
2024-01-04 12:45:22 -0500

    created react client

--- server/index.js
+++ server/index.js
@@ -7,11 +7,21 @@ const Book = require('./models/Books');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
 connectDB();
 app.use(cors());
 app.use(express.urlencoded( {extended: true}));
 app.use(express.json());
+
+
+app.get("/api/books", async (req, res) => {
+    try {
+        const data = await Book.find({});
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while fetching books."});
+    }
+});
+
 
 
 app.get("/", (req, res) => {
