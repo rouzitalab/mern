f4658775358f128552076e85354e96f878c97cbc
Alireza Rouzitalab <alireza@alirezas-air.home>
2024-01-09 14:48:10 -0500

    app changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,22 +3,23 @@ import Home from "./routes/Home/home";
 import About from "./routes/About/about";
 import Book from "./routes/Book/book";
 import SingleBook from "./routes/Book/singleBook";
+import CreateBook from "./routes/Book/createBook";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
 
 function App() {
   return (
     <>
       <Router>
-        <Header/>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/books" element={<Book />} />
           <Route path="/books/:slug" element={<SingleBook />} />
+          <Route path="/createbook" element={<CreateBook />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </>
   );
